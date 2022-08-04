453290d9fd2a77ba62c0c4b31889a43f93f818b3
Srikanth Racharla <srikanth@Srikanths-MacBook-Pro.local>
2022-08-04 10:33:54 +0530

    navigation setup completed

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import IndexScreen from "./src/screens/IndexScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    IndexScreen: IndexScreen,
   },
-});
+  {
+    initialRouteName: "IndexScreen",
+    defaultNavigationOptions: {
+      title: "Blog Posts",
+    },
+  }
+);
+
+const App = createAppContainer(navigator);
+
+export default () => {
+  return <App />;
+};
